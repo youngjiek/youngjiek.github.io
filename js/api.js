fd4da09ff8244dbc855f48d571403c9b7938cbc2
youngjiek <865100300@qq.com>
2022-08-01 22:00:33 +0800

    提交

---
+++ js/api.js
@@ -0,0 +1,26 @@
+//配置
+
+//服务端地址
+var base_url = "http://localhost:8002";
+
+
+
+//自己封装一个ajax请求函数
+function requestPost(url,data={},callback) {
+    $.ajax({
+        url:url,
+        method:'POST',
+        data:data,
+        success:function (ret) {
+            if(ret.code==0){//0成功,-1报错
+                callback(true,ret.data);
+            }else{
+                callback(false,ret.msg);
+            }
+        },
+        error:function (ret) {
+            callback(false,'网络错误');
+        }
+    });
+    return;
+}
